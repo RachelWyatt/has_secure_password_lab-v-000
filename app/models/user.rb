a136f4083f935d21220e48af0b4ef1ce5a6f8578
Rachel Wyatt <rw126@evansville.edu>
2019-03-12 12:39:16 +0000

    Automatically backed up by Learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,4 @@
 class User < ActiveRecord::Base
   has_secure_password
 
-  def password=(new_password)
-    salt = BCrypt::Engine::generate_salt
-    hashed = BCrypt::Engine::hash_secret(new_password, salt)
-    self.password_digest = salt + hashed
-  end
 end
