d540610c2ec7457c39b12f504055df753025052d
Rachel Wyatt <rw126@evansville.edu>
2019-03-12 12:53:17 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :sessions, :users
+  resources :users
+  get '/', to: "sessions#index", as: "root"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
